2c5d33e4948888c20e468142b8246ebb6fcae9dc
Steellow <hankilapio@gmail.com>
2021-04-23 10:52:20 +0300

    Create header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-function App() {
+import Header from "./components/Header";
+
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="App mx-auto p-6 max-w-4xl">
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="border-b border-gray-300 pb-6">
+      <a href="$" className="font-bold hover:text-red-600 active:text-red-700 text-2xl">
+        Super awesome blog
+      </a>
+      {/* TODO: Search bar */}
+    </header>
+  );
+};
+
+export default Header;
